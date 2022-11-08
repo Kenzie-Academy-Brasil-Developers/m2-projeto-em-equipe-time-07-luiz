dd70da8a3c07de5844bd89329d4a685c653ff0c6
Bryan <bryanbatista79688@gmail.com>
2022-11-08 11:19:45 -0300

    Feat: complete register functions

--- src/scripts/register/register.js
+++ src/scripts/register/register.js
@@ -1,44 +1,46 @@
+const form = document.querySelector('.form-container');
+
 const accountWithApi = async (body, url) => {
 
-  try {
+  const request = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  })
 
-    const request = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body)
-    })
-
-    const result = await request.json();
-    return result;
-
-  }
-  catch (err) {
-    console.log(err);
-  }
-
+  const result = await request.json();
+  console.log(result)
+  return result;
 }
 
 const getInputs = () => {
 
-  const form = document.querySelector('.form-container');
   const formElements = [...form.elements];
-
-  console.log(formElements)
-
   formElements.pop();
-
-  console.log(formElements)
-
   return formElements;
 
 }
 
-getInputs()
-
 const registerData = () => {
 
-  
+  const inputArray = getInputs();
 
+  form.addEventListener('submit', async (e) => {
+
+    e.preventDefault();
+
+    const [name, email, password, avatar] = inputArray;
+
+    const registerUserData = {};
+    registerUserData.name     = name.value;
+    registerUserData.email    = email.value;
+    registerUserData.password = password.value;
+    registerUserData.avatar_url   = avatar.value;
+
+    accountWithApi(registerUserData, 'https://m2-api-adot-pet.herokuapp.com/users');
+  })
 }
+
+registerData();
 
 export { accountWithApi };
