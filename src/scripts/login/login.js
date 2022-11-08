a6cb396a9341cc05510ec0034e63c86d63600054
Sidnei Miolli Junior <juniormiolli@hotmail.com>
2022-11-08 13:50:46 -0300

    feat confirmed value input != from empty to click on login button

--- src/scripts/login/login.js
+++ src/scripts/login/login.js
@@ -1,28 +1,67 @@
 let email = document.getElementById('email')
-let senha = document.getElementById('senha')
+let password = document.getElementById('senha')
 let login = document.getElementById('login')
+let register = document.getElementById('register')
+let span = document.getElementById('span')
 
 
-const realizarLogin = async () =>{
+const access = async () =>{
 
-    const dados = {
+    const data = {
         "email" : `${email.value}`,
-        "password" : `${senha.value}`
+        "password" : `${password.value}`
     }
     const request = {
         method:'POST',
         headers:{
             'Content-Type':'application/json'
         },
-        body:JSON.stringify(dados)
+        body:JSON.stringify(data)
     }
 
-    const requisicaoJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/session/login',request)
-    const requisicao = await requisicaoJSON.json()
-    console.log(requisicaoJSON)
-    console.log(requisicao)
+    const responseJSON = await fetch('https://m2-api-adot-pet.herokuapp.com/session/login',request)
+    const response = await responseJSON.json()
+
+    if(responseJSON.ok == true){
+
+        localStorage.setItem('token',response.token)
+        localStorage.setItem('name',response.user.name)
+        localStorage.setItem('img',response.user.avatar_url)
+
+        setTimeout(()=>{
+            window.location.href = '../pages/homeLogged-in.html'
+        },1000)
+    }else{
+        span.innerText = 'Usuário ou senha incorreto'
+        email.focus()
+    }
+
 }
 
-login.addEventListener('click',function(){
-    realizarLogin()
+
+const checkInput = (email,password) =>{
+
+    if(email.value != '' && password.value != ''){
+        login.disabled = false
+    }else{
+        login.disabled = true
+    }
+}
+
+email.addEventListener('input', () =>{
+    checkInput(email,password)
+})
+password.addEventListener('input', () =>{
+    checkInput(email,password)
+})
+
+login.addEventListener('click', () =>{
+        if(email.value != '' && password.value != ''){
+            access()
+        }else{
+            alert('Preencha os campos')
+        }
+})
+register.addEventListener('click', () => {
+    window.location.href = '../pages/register.html'
 })
